e8398ac9ce70526784b6cd39df16fcacfa710924
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-11-10 00:23:49 +0200

    adding some assets

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import Logo from "../assets/logo.png";
+
+const NavbarMenu = [
+    {
+      id: 1,
+      title: "Home",
+      link: "#",
+    },
+    {
+      id: 2,
+      title: "Categories",
+      link: "#",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      link: "#",
+    },
+    {
+      id: 4,
+      title: "About",
+      link: "#",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      link: "#",
+    },
+  ];
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-orange-500 text-white py-8'>
+      <div className='container'>
+       <div>
+        <img src={Logo} alt="Fanta" className='max-w-[100px] invert'>
+         
+        </img>
+       </div>
+      </div>
+    </div>
   )
 }
 
