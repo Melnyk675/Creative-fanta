e29db76eafd006c464727e3380d2f28718ee7261
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-11-10 16:45:30 +0200

    adding Navbar section

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Logo from "../assets/logo.png";
+import { FaUserLarge } from "react-icons/fa6";
+import { RiMenuUnfold4Line } from "react-icons/ri";
 
 const NavbarMenu = [
     {
@@ -32,14 +34,30 @@ const NavbarMenu = [
 const Navbar = () => {
   return (
     <div className='bg-orange-500 text-white py-8'>
-      <div className='container'>
+      <div className='container flex justify-between'>
        <div>
-        <img src={Logo} alt="Fanta" className='max-w-[100px] invert'>
-         
-        </img>
-       </div>
-      </div>
+        <img src={Logo} alt="Fanta" className='max-w-[100px] invert'/>
+        </div>
+     <div className='hidden md:block'>
+        <ul className='flex items-center gap-4 relative z-30'>
+            {NavbarMenu.map((item) => (
+           <li>
+            <a 
+            href={item.link}
+            className='inline-block text-base font-semibold py-2 px-3 uppercase'
+            >{item.title}</a>
+           </li>
+         ))}
+         <button className='text-xl ps-14'>
+          <FaUserLarge />
+         </button>
+       </ul>
+     </div>
+     <div className='md:hidden'> 
+      <RiMenuUnfold4Line />
+     </div>
     </div>
+   </div>
   )
 }
 
