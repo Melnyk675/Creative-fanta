803982933ed1e21ccdf49ea0a2355e8b87482d6d
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-11-11 17:35:11 +0200

    commit

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Fanta1 from "../assets/fanta1.png";
 import Fanta2 from "../assets/fanta2.png";
 import Fanta3 from "../assets/fanta3.png";
+import {motion} from "framer-motion";
+import Navbar from './Navbar';
 
 const headphoneData = [
     {
@@ -45,11 +47,32 @@ const Hero = () => {
 
   return (
     <>
-     <div>
-        <div className='container'>
+     <motion.div 
+      initial={{ backgroundColor: activeData.bgColor }}
+      animate={{ backgroundColor: activeData.bgColor }}
+      transition={{ duration: 0.8 }}
+      className="bg-brandDark text-white"
+      >
+     
+       <div className='container grid grid-cols-1 md:grid-cols-2 min-h-[605px]'>
+        <div className='flex flex-col justify-center py-14 md:py-0 xl:max-w-[500px] order-2 md:order-1'>
+         <div className='space-y-5 text-center md:text-left'>
+          <h1 className='text-3xl lg:text-6xl xl:text-7xl font-bold font-handwriting text-shadow'>{activeData.title}</h1>
+          <p className=''>{activeData.subtitle}</p>
+          <button className='px-4 py-2 text-black bg-white inline-block font-normal rounded-sm'>
+            Order Now
+          </button>
 
+          <div className='flex items-center justify-center md:justify-start gap-4 !md:mt-24 !mb-10'>
+            <div className='w-20 h-[1px] bg-white'></div>
+            <p>TOP RECOMMENDATION</p>
+            <div className='w-20 h-[1px] bg-white'></div>
+          </div>
+
+         </div>  
         </div>
-      </div>
+       </div>
+      </motion.div>
     </>
   )
 }
