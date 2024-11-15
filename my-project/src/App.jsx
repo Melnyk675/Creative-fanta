9f1adf285b5e9b4a72efc391a52b28bfe53af6a3
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-11-15 21:38:52 +0200

    creating Products section

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,12 @@
 
 import Hero from "./components/Hero";
+import Products from "./components/Products/Products";
 
 const App = () => {
   return <main className="overflow-x-hidden">
       
       <Hero />
+      <Products />
     </main>
 };
 
