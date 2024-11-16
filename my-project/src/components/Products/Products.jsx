f742ebfeae6d892719d5b1dca8eb45b501496ff7
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-11-16 15:35:49 +0200

    adding Products section

--- my-project/src/components/Products/Products.jsx
+++ my-project/src/components/Products/Products.jsx
@@ -53,11 +53,36 @@ const Products = () => {
         variants={fadeUp(0.2)}
         initial="hidden"
         whileInView="show"
-        className='text-3xl font-bold text-center pb-10'>
+        className='text-3xl font-bold text-center pb-10'
+        >
         Our Products
      </motion.h1>
-       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
-
+     
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          {ProductsData.map((item) => (
+            <motion.div
+              variants={fadeUp(item.delay)}
+              key={item.id}
+              initial="hidden"
+              whileInView={"show"}
+              className="flex flex-col items-center justify-center p-5 max-w-[300px] mx-auto shadow-lg rounded-xl bg-white"
+            >
+              <img
+                src={item.image}
+                alt=""
+                className="w-[100px] mb-4 hover:rotate-12 hover:scale-110 duration-300"
+              />
+              <div className="text-center space-y-2">
+                <h1 className="text-2xl font-bold font-handwriting text-center">
+                  {item.title}
+                </h1>
+                <p className="text-center text-sm text-gray-600">{item.desc}</p>
+                <button className="!mt-5 border-2 border-primary text-primary px-6 py-2 rounded-md hover:bg-primary hover:text-white duration-200">
+                  Buy Now
+                </button>
+              </div>
+            </motion.div>
+          ))}
        </div>
      </div>
     </div>
