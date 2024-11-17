174ff38b00b0fa30a57e376d9d29e2a3f2536b94
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2024-11-17 15:01:04 +0200

    commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,14 @@
 
 import Hero from "./components/Hero";
 import Products from "./components/Products/Products";
+import Banner from "./components/Banner/Banner";
 
 const App = () => {
   return <main className="overflow-x-hidden">
       
       <Hero />
       <Products />
+      <Banner />
     </main>
 };
 
